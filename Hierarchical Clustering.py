fe996aedc6ca748c8bcd07c0db495e960e33ec50
Ivo196 <ivo.tonioni@gmail.com>
2023-12-26 19:08:36 +0100

    first commit

---
+++ Hierarchical Clustering.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 21:24:19 2023
+
+@author: ivoto
+"""
+
+#Hierarchical Clustering (Clustering Jerarquico)
+
+# Importar las librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Cargamos los datos con pandas
+dataset = pd.read_csv("Mall_Customers.csv")
+X = dataset.iloc[:, [3,4]].values
+
+#Utilizar el dendrograma para encontrar el numero optimo de cluster 
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = "ward")) 
+plt.title("Dendrograma")
+plt.xlabel("Clientes")
+plt.ylabel("Distancia Euclidea")
+plt.show()
+
+#Ajustar el clustering jerarquico a nuestro conjunto de datos
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = "euclidean", linkage = "ward")
+y_hc = hc.fit_predict(X)
+
+# Visualización de los clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = "red", label = "Cautos")
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = "blue", label = "Estandard")
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = "green", label = "Objetivo")
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = "cyan", label = "Descuidados")
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = "magenta", label = "Conservadores")
+plt.title("Cluster de clientes")
+plt.xlabel("Ingresos anuales (en miles de $)")
+plt.ylabel("Puntuación de Gastos (1-100)")
+plt.legend()
+plt.show()
